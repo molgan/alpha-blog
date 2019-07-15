6b5ca0c07015911ff8b1cfdc920c993b2833e090
molgan <olga.matsuga@gmail.com>
2019-07-15 22:09:46 +0000

    Add edit, listing, destroy actions + add partials

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,17 @@
 class ArticlesController < ApplicationController
   
+  def index
+    @articles = Article.all
+  end
+  
   def new
     @article = Article.new
   end
   
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def create
     # render plain: params[:article].inspect
     @article = Article.new(article_params)
@@ -15,8 +23,25 @@ class ArticlesController < ApplicationController
     end
   end
   
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = 'Article was successufully updated'
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+  end
+  
   def show
     @article = Article.find(params[:id])
+  end
+  
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash[:notice] = 'Article was successufully deleted'
+    redirect_to articles_path
   end
   
   private
